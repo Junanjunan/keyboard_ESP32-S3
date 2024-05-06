86515129fdf9c786057085730ba03643a54e8e21
Junanjunan <taltalrealty@gmail.com>
2024-05-06 21:21:19 +0900

    mode_change: [add] detect current mode in whileloop

--- examples/mode_change/main/main.c
+++ examples/mode_change/main/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "nvs_flash.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #include "mode_gpio.h"
 
 
 void app_main() {
+    connection_mode_t mode = 0;
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -14,6 +17,12 @@ void app_main() {
     }
 
     setup_mode_gpio();
-    connection_mode_t mode = detect_current_mode();
-    ESP_LOGI("MODE", "Current mode: %d (0: USB, 1: BLE, 2: Wireless)", mode);
+    while (1) {
+        connection_mode_t current_mode = detect_current_mode();
+        if (current_mode != mode) {
+            mode = current_mode;
+            ESP_LOGI("MODE", "changed mode: %d (0: USB, 1: BLE, 2: Wireless)", mode);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
