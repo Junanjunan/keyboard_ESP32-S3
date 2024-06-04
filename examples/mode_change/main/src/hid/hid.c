a8917da156282ea2a81f523984a5fc86329719d0
Junanjunan <taltalrealty@gmail.com>
2024-06-05 01:31:14 +0900

    mode_change: [add] Combination key event 1

--- examples/mode_change/main/src/hid/hid.c
+++ examples/mode_change/main/src/hid/hid.c
@@ -62,7 +62,7 @@ uint8_t keycodes[6][18] = {
     {HID_KEY_GRAVE,         HID_KEY_1,          HID_KEY_2,         HID_KEY_3,          HID_KEY_4,              HID_KEY_5,       HID_KEY_6,           HID_KEY_7,         HID_KEY_8,      HID_KEY_9,          HID_KEY_0,              HID_KEY_MINUS,          HID_KEY_EQUAL,              HID_KEY_NONE,       HID_KEY_BACKSPACE,      HID_KEY_INSERT,         HID_KEY_HOME,           HID_KEY_PAGE_UP},
     {HID_KEY_TAB,           HID_KEY_Q,          HID_KEY_W,         HID_KEY_E,          HID_KEY_R,              HID_KEY_T,       HID_KEY_Y,           HID_KEY_U,         HID_KEY_I,      HID_KEY_O,          HID_KEY_P,              HID_KEY_BRACKET_LEFT,   HID_KEY_BRACKET_RIGHT,      HID_KEY_NONE,       HID_KEY_BACKSLASH,      HID_KEY_DELETE,         HID_KEY_END,            HID_KEY_PAGE_DOWN},
     {HID_KEY_CAPS_LOCK,     HID_KEY_A,          HID_KEY_S,         HID_KEY_D,          HID_KEY_F,              HID_KEY_G,       HID_KEY_H,           HID_KEY_J,         HID_KEY_K,      HID_KEY_L,          HID_KEY_SEMICOLON,      HID_KEY_APOSTROPHE,     HID_KEY_NONE,               HID_KEY_NONE,       HID_KEY_ENTER,          HID_KEY_NONE,           HID_KEY_NONE,           HID_KEY_NONE},
-    {HID_KEY_SHIFT_LEFT,    HID_KEY_Z,          HID_KEY_X,         HID_KEY_C,          HID_KEY_V,              HID_KEY_B,       HID_KEY_N,           HID_KEY_M,         HID_KEY_COMMA,  HID_KEY_PERIOD,     HID_KEY_SLASH,          HID_KEY_NONE,           HID_KEY_NONE,               HID_KEY_NONE,       HID_KEY_SHIFT_RIGHT,    HID_KEY_NONE,           HID_KEY_ARROW_UP,       HID_KEY_NONE},
+    {KEYBOARD_MODIFIER_LEFTSHIFT,    HID_KEY_Z,          HID_KEY_X,         HID_KEY_C,          HID_KEY_V,              HID_KEY_B,       HID_KEY_N,           HID_KEY_M,         HID_KEY_COMMA,  HID_KEY_PERIOD,     HID_KEY_SLASH,          HID_KEY_NONE,           HID_KEY_NONE,               HID_KEY_NONE,       HID_KEY_SHIFT_RIGHT,    HID_KEY_NONE,           HID_KEY_ARROW_UP,       HID_KEY_NONE},
     {HID_KEY_CONTROL_LEFT,  HID_KEY_GUI_LEFT,   HID_KEY_ALT_LEFT,  HID_KEY_NONE,       HID_KEY_NONE,           HID_KEY_SPACE,   HID_KEY_NONE,        HID_KEY_NONE,      HID_KEY_NONE,   HID_KEY_ALT_RIGHT,  HID_KEY_GUI_RIGHT,      HID_KEY_APPLICATION,    HID_KEY_NONE,               HID_KEY_NONE,       HID_KEY_CONTROL_RIGHT,  HID_KEY_ARROW_LEFT,     HID_KEY_ARROW_DOWN,     HID_KEY_ARROW_RIGHT}
 };
 
@@ -157,7 +157,28 @@ keyboard_btn_cb_config_t cb_cfg = {
 };
 
 
+static void keyboard_combination_cb1(keyboard_btn_handle_t kbd_handle, keyboard_btn_report_t kbd_report, void *user_data)
+{
+    printf("combination 1: ");
+    for (int i = 0; i < kbd_report.key_pressed_num; i++) {
+        printf("(%d,%d) ", kbd_report.key_data[i].output_index, kbd_report.key_data[i].input_index);
+    }
+    printf("\n\n");
+}
+
+keyboard_btn_cb_config_t cb_cfg_combi = {
+    .event = KBD_EVENT_COMBINATION,
+    .callback = keyboard_combination_cb1,
+    .event_data.combination.key_num = 2,
+    .event_data.combination.key_data = (keyboard_btn_data_t[]) {
+        {5, 1},
+        {1, 1},
+    },
+};
+
+
 void keyboard_task(void) {
     keyboard_button_create(&cfg, &kbd_handle);
     keyboard_button_register_cb(kbd_handle, cb_cfg, NULL);
+    keyboard_button_register_cb(kbd_handle, cb_cfg_combi, NULL);
 }
