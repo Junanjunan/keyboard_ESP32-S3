54e131294d0f92a061e6257b3a8ef86c6d06e753
Junanjunan <taltalrealty@gmail.com>
2024-06-11 02:02:06 +0900

    mode_change: [add] Log about pressed keycode with hexadecimal

--- examples/mode_change/main/src/hid/hid.c
+++ examples/mode_change/main/src/hid/hid.c
@@ -81,6 +81,7 @@ void keyboard_cb(keyboard_btn_handle_t kbd_handle, keyboard_btn_report_t kbd_rep
 
     for (int i = 0; i < kbd_report.key_pressed_num; i++) {
         keycode = keycodes[kbd_report.key_data[i].output_index][kbd_report.key_data[i].input_index];
+        ESP_LOGI(__func__, "pressed_keycode: %x", keycode);
         if (is_modifier(keycode)) {
             modifier |= keycode;
             keycode = 0;
